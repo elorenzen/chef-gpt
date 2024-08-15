97d2e360061b01f6e7f8ad5855254cb181468863
Eric Lorenzen <eric.lorenzen@gmail.com>
2024-08-15 12:19:36 -0700

    Add basic recipe retrieval func using Spoonacular API

--- components/Embedding.vue
+++ components/Embedding.vue
@@ -1,11 +1,16 @@
 <template>
     <div>
         <div class="flex justify-center p-2">
-            <UTextarea placeholder="Enter your ingredients..." v-model="embeddings"></UTextarea>
+            <UTextarea placeholder="Enter your ingredients..." v-model="input"></UTextarea>
         </div>
-            <div class="flex justify-center py-2">
-            <UButton @click="showIngredients">Button</UButton>
+        <div class="flex justify-center px-2">
+            <div class="m-2"><UButton color="indigo" @click="clear">Clear</UButton></div>
+            <div class="m-2"><UButton @click="getRecipes">Get recipes</UButton></div>
         </div>
+
+        <UCard v-if="recipes !== '' && recipes.length > 0" v-for="recipe in recipes" :key="recipe.uri">
+            {{ recipe }}
+        </UCard>
     </div>
 </template>
 
@@ -13,10 +18,34 @@
 import { ref } from 'vue'
 export default {
     setup () {
-        const embeddings = ref('')
-        function showIngredients() { console.log (embeddings.value) }
+        const config = useRuntimeConfig()
 
-        return { embeddings, showIngredients }
+        const input = ref('')
+        const recipes = ref('')
+
+        function clear() { input.value = '' }
+        const getRecipes = async () => {
+            const url = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${config.public.foodId}&number=50&ranking=1&ignorePantry=true&ingredients=`
+
+            const parsedIngredients = input.value.split(',')
+            let paramStr = ''
+            parsedIngredients.forEach((ing, idx) => {
+                idx + 1 < parsedIngredients.length ?
+                    paramStr += `${ing},` :
+                    paramStr += ing
+            });
+            
+            const res = await $fetch(`${url}${paramStr}`, { method: 'GET' })
+            console.log('RES: ', res)
+        }
+        
+
+        return {
+            input,
+            recipes,
+            getRecipes,
+            clear
+        }
     }
 }
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@nuxtjs/tailwindcss"]
+  modules: ["@nuxt/ui", "@nuxtjs/tailwindcss"],
+  runtimeConfig: {
+    public: {
+      foodId: process.env.FOOD_KEY
+    }
+  }
 })
