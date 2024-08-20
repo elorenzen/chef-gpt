a7a19260000307cc2eaf2e28ce8b512785ef6974
Eric Lorenzen <eric.lorenzen@gmail.com>
2024-08-19 18:16:47 -0700

    UI improvements

--- components/Embedding.vue
+++ components/Embedding.vue
@@ -8,9 +8,49 @@
             <div class="m-2"><UButton @click="getRecipes">Get recipes</UButton></div>
         </div>
 
-        <UCard v-if="recipes !== '' && recipes.length > 0" v-for="recipe in recipes" :key="recipe.uri">
-            {{ recipe }}
-        </UCard>
+        <v-card class="ma-2 py-2" color="#BF360C" v-if="recipes !== '' && recipes.length > 0" v-for="recipe in recipes" :key="recipe.uri">
+          <v-row>
+            <v-col cols="10">
+              <v-card-title class="text-h5">{{ recipe.title }}</v-card-title>
+              <v-row>
+                <v-col cols="6">
+                    <v-card-subtitle>Used ingredients: </v-card-subtitle>
+                </v-col>
+                <v-col cols="6">
+                    <v-card-subtitle>Needed ingredients: </v-card-subtitle>
+                </v-col>
+              </v-row>
+
+              <!-- <v-card-actions>
+                <v-btn
+                  class="ms-2"
+                  size="small"
+                  text="START RADIO"
+                  variant="outlined"
+                ></v-btn>
+              </v-card-actions> -->
+            </v-col>
+
+            <v-avatar
+              class="ma-3"
+              rounded="sm"
+              size="125"
+            >
+              <v-img class="rounded-":src="recipe.image"></v-img>
+            </v-avatar>
+
+            <!-- TURN TO EXPANSION PANEL -->
+            <!-- <v-btn
+                class="ma-2"
+                color="primary"
+                block
+                variant="text"
+            >
+                View Recipe
+                <v-icon icon="mdi-arrow-down" end></v-icon>
+            </v-btn> -->
+          </v-row>
+        </v-card>
     </div>
 </template>
 
@@ -25,7 +65,7 @@ export default {
 
         function clear() { input.value = '' }
         const getRecipes = async () => {
-            const url = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${config.public.foodId}&number=50&ranking=1&ignorePantry=true&ingredients=`
+            const url = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${config.public.foodId}&number=5&ranking=1&ignorePantry=true&ingredients=`
 
             const parsedIngredients = input.value.split(',')
             let paramStr = ''
@@ -35,7 +75,7 @@ export default {
                     paramStr += ing
             });
             
-            const res = await $fetch(`${url}${paramStr}`, { method: 'GET' })
+            recipes.value = await $fetch(`${url}${paramStr}`, { method: 'GET' })
             console.log('RES: ', res)
         }
         
