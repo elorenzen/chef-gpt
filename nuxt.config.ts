1b4864cbc701b441b0086ee27768a3a108b50dd5
Eric Lorenzen <eric.lorenzen@gmail.com>
2024-08-19 18:16:13 -0700

    Add Vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@nuxtjs/tailwindcss", '@nuxtjs/supabase'],
+  modules: [
+    "@nuxt/ui",
+    "@nuxtjs/tailwindcss",
+    '@nuxtjs/supabase',
+    "@nuxt/image",
+    "vuetify-nuxt-module"
+  ],
   runtimeConfig: {
     public: {
       foodId: process.env.FOOD_KEY
@@ -12,5 +18,25 @@ export default defineNuxtConfig({
     url: process.env.SUPABASE_URL,
     key: process.env.SUPABASE_KEY,
     redirect: false // set to 'true', EVENTUALLY
+  },
+  vuetify: {
+    vuetifyOptions: {
+      components: [
+        'VDialog',
+        'VCard',
+        'VCardTitle',
+        'VBtn',
+        'VCardSubtitle',
+        'VImg',
+        'VAvatar',
+        'VCardActions',
+        'VList',
+        'VListItem',
+        'VIcon',
+        'VListItemTitle',
+        'VRow',
+        'VCol'
+      ]
+    }
   }
 })
